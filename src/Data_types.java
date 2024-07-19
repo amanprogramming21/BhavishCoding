867a1675cd8435df7cdcba1b0d40e53757960800
Bhavish089 <72312465+Bhavish089@users.noreply.github.com>
2024-07-19 20:39:19 +0530

    Increment / Decrement

---
+++ src/Data_types.java
@@ -0,0 +1,13 @@
+public class Data_types {
+    public static void main(String[] args) {
+        int a;
+        System.out.println("Increment");
+        for(a=0;a<51;a++) {
+            System.out.println(a);
+        }
+        System.out.println("\nDecrement");
+        for (a=51;a>0;a--) {
+            System.out.println(a);
+        }
+    }
+}
