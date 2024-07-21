274e4292e2914e16c436a942c12e25ebfa597640
Bhavish089 <72312465+Bhavish089@users.noreply.github.com>
2024-07-21 16:31:17 +0530

    Practice Question 3

---
+++ src/Exersice_three.java
@@ -0,0 +1,23 @@
+public class Exersice_three {
+    public static void main(String[] args) {
+        /* Question-1 */
+        String a = "HELLO";
+        System.out.println(a.toLowerCase());
+
+        /* Question-2 */
+        String b = "My Name";
+        System.out.println(b.replace(' ','_'));
+
+        /* Question-3 */
+        String c = "Hi, I am name nice to meet you";
+        System.out.println(c.replace("name","Admin"));
+
+        /* Question-4 */
+        String d = "Hello world this is Java";
+        System.out.println(d.indexOf(" ",11));
+
+        /* Question-5 */
+        String e = "Java is like C and C++";
+        System.out.println("\"" + e + "\"");
+    }
+}
