aab7a931ba31f2e603dd79ceb8d2a15c7afdb80a
Bhavish089 <72312465+Bhavish089@users.noreply.github.com>
2024-07-22 11:53:48 +0530

    giving wrong number of results

--- src/Random_number_generator.java
+++ src/Random_number_generator.java
@@ -1,11 +1,18 @@
 import java.util.Random;
+import java.util.Scanner;
 import java.util.random.RandomGenerator;
 
 public class Random_number_generator {
     public static void main(String[] args) {
-        Random random = new Random();
-        int x = random.nextInt();
+        Scanner UC = new Scanner(System.in);
+        System.out.print("Enter how many number you want to generate: ");
+        int UC1 = UC.nextInt();
+        while(UC1 >= 0) {
+            Random random = new Random();
+            int x = random.nextInt();
 
-        System.out.println(x);
+            System.out.println(x);
+            --UC1;
+        }
     }
 }
