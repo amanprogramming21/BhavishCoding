e84ca8037c0ea44f28fbda401296d311dbf2488e
Bhavish089 <72312465+Bhavish089@users.noreply.github.com>
2024-07-30 12:40:45 +0530

    update

--- src/Array.java
+++ src/Array.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Array {
     public static void main(String[] args) {
-        int [] marks = new int[5];
+        int[] marks = new int[5];
         System.out.println("Enter number to store in array");
 
         marks[0] = 50;
@@ -12,5 +14,23 @@ public class Array {
         for (int element : marks) {
             System.out.println(element);
         }
-}
+
+        System.out.println("\n\t\tUser Input");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many number will you enter: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Great! enter " + n + "float :");
+
+        float[] a = new float[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextFloat();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
 }
