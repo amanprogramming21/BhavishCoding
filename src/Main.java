b5ad7970010cc4f83c5d1d95370b30a7a5d70590
Bhavish089 <72312465+Bhavish089@users.noreply.github.com>
2024-08-06 09:23:00 +0530

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n\tHello and Welcome");
+        System.out.println("\n\tThis file allow you Experience other Java File");
+        Scanner JAccess = new Scanner(System.in);
+        System.out.println("Select Java file you want to");
     }
 }
